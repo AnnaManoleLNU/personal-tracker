26793c1b1b2ddd55114151cc67ace5b3fa48e702
Anna Manole <am224wd@student.lnu.se>
2024-07-06 21:27:11 +0200

    remove unnecessary stuff

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,12 @@ import { Calendar } from "@nextui-org/calendar";
 import {today, getLocalTimeZone} from "@internationalized/date";
 
 export default function Home() {
-  const fullDate = new Date();
-  const date = fullDate.getDate();
   return (
     <section>
       <Calendar
         aria-label="Date (Uncontrolled)"
         defaultValue={today(getLocalTimeZone())}
+        color="primary"
       />
     </section>
   );
